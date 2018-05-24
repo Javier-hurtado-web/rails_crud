b30713486fc36583201e1e8c8a36b24084e895c9
Javier-hurtado-web <javier.hurtdaowebmaster@gmail.com>
2018-05-24 16:41:36 +0200

    better errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
+
 get '/article/new', to: 'articles#new'
 post '/article/create', to: 'articles#create'
 get '/article/:id', to: 'articles#show', as:'new_article'
