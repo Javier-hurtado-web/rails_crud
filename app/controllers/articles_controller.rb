8da04c7fee43a1324c22031253867515cc211c42
Javier-hurtado-web <javier.hurtdaowebmaster@gmail.com>
2018-05-24 13:32:25 +0200

    dokončený projekt

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+    
+    def index
+    	@articles = Article.all
+    end
+
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+	    @article = Article.new(permited)
+	    if @article.save
+	    redirect_to new_article_path(@article)
+	    else
+	    	render 'new'
+	    end
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+
+     def edit
+     	@article = Article.find(params[:id])
+     end 
+
+
+     def update
+
+     	article = Article.find(params[:id])
+        article.update(permited)
+        redirect_to new_article_path(article)
+     	
+     end
+    def destroy
+    	article = Article.find(params[:id])
+        article.destroy
+        redirect_to articles_path
+    end
+
+	private 
+
+	def permited
+		params.require(:article).permit(:title, :body)
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+get '/article/new', to: 'articles#new'
+post '/article/create', to: 'articles#create'
+get '/article/:id', to: 'articles#show', as:'new_article'
+get '/articles/', to: 'articles#index', as: 'articles'
+get '/article/:id/edit', to: 'articles#edit', as: 'article_edit'
+patch '/article/:id/', to: 'articles#update'
+put '/article/:id/', to: 'articles#update'
+delete '/article/:id/', to: 'articles#destroy', as: 'article_delete'
+
+
+
+end
