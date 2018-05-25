9b70d18b44ef69c0e642824d95c6e28b6f3ecf29
Javier-hurtado-web <javier.hurtdaowebmaster@gmail.com>
2018-05-25 16:04:41 +0200

    nastavení vytvoření image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,7 +3,9 @@ class ImagesController < ApplicationController
 
     def create
        article = Article.find(params[:id])
-       images = article.image.new(permited)
+       images = article.image.new()
+       images.url = permited[:url][0].original_filename
+       images.article_id = params[:id]
        images.save
        redirect_to new_article_path(article)
     end
