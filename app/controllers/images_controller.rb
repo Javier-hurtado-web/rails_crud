2ab7a05817e2a1e23e380776b761262dcf3c7322
Javier-hurtado-web <javier.hurtdaowebmaster@gmail.com>
2018-05-28 11:34:36 +0200

    upload fotek do public / uploads

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -7,7 +7,18 @@ class ImagesController < ApplicationController
        images.url = permited[:url][0].original_filename
        images.article_id = params[:id]
        images.save
+       
+       uploaded_io = permited[:url][0]
+       File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+         file.write(uploaded_io.read)
+       end
+
+
+      
+    
+
        redirect_to new_article_path(article)
+
     end
    
     private 
