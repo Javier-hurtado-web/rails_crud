32cc0e35bff017be4a130d793d026e11e0a0207f
Javier-hurtado-web <javier.hurtdaowebmaster@gmail.com>
2018-06-01 12:13:09 +0200

    vlastní test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,12 +1,14 @@
 require "rails_helper"
 
-RSpec.describe Article, :type => :model do
-  context "with 2 or more comments" do
-    it "orders them in reverse chronologically" do
-      article = Article.create!(body: "Nejaké body", title: "nějaký text")
-      post1 = article.post.create!(text: "first comment")
-      post2 = article.post.create!(text: "second comment")
-      expect(article.post).to eq([post1, post2])
+describe "Article" do
+  context "zadáme článek" do
+    it "s očekávanou hodnotou" do
+      article = Article.create!(body: "Body text", title: "nějaký text")
+      expect(article.body).to eq "Body text"
+    end
+    it "vrátí id článku" do
+      article = Article.create!(body: "Body text", title: "nějaký text")
+      expect(article.id).to eq article.id
     end
   end
 end
